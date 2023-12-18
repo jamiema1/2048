bda5ad66d02681bd318b4110874d77489a8ec728
Jamie Ma <jkmm.1024@gmail.com>
2023-12-17 21:24:24 -0800

    Made grid

--- src/main.py
+++ src/main.py
@@ -7,33 +7,43 @@ pygame.display.set_caption('2048')
 # pygame.display.set_icon()
 
 SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
+SCREEN_HEIGHT = 800
+
+PADDING = 10
+SQUARE_SIZE = 50
+
+COLOUR_1 = (255, 223, 107)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-player = pygame.Rect((50,50,50,50))
+tiles = []
+
+# startTile = pygame.Rect((PADDING,PADDING,SQUARE_SIZE,SQUARE_SIZE))
+
+# tiles.append(startTile)
 
 run = True
 
 BACKGROUND_COLOUR = (0,0,0)
 
+for x in range(4):
+    for y in range(4):
+        startTile = pygame.Rect((PADDING + x * (PADDING + SQUARE_SIZE),PADDING + y * (PADDING + SQUARE_SIZE),SQUARE_SIZE,SQUARE_SIZE))
+        tiles.append(startTile)
+
+
 while run:
     
     screen.fill(BACKGROUND_COLOUR)
     
-    pygame.draw.rect(screen, (255, 0, 0), player)
+    for tile in tiles:
+        pygame.draw.rect(screen, COLOUR_1, tile)
     
     key = pygame.key.get_pressed()
     
-    amount = 1
-    if key[pygame.K_w] == True:
-        player.move_ip(0, -amount)
-    elif key[pygame.K_a] == True:
-        player.move_ip(-amount, 0)
-    elif key[pygame.K_s] == True:
-        player.move_ip(0, amount)
-    elif key[pygame.K_d] == True:
-        player.move_ip(amount, 0)
+    # if key[pygame.K_s] == True:
+    #     square.move_ip(0, 1)
+    
     
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
